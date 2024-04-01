0f72d96eefe92d3017f2430306612c9c54ebfe76
vadym <vasylaki95@gmail.com>
2024-04-01 15:16:07 +0300

    Initial Commit

---
+++ FigmaTestProj/FigmaTestProjApp.swift
@@ -0,0 +1,17 @@
+//
+//  FigmaTestProjApp.swift
+//  FigmaTestProj
+//
+//  Created by Vadym Vasylaki on 01.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct FigmaTestProjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
